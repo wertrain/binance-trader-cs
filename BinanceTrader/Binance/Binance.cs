b67f2bf2c150d20cdccd31dcc90cf28ecb69ca92
wertrain <wertrain@gmail.com>
2021-03-14 11:26:02 +0900

    アプリケーションのひな型を作成

---
+++ BinanceTrader/Binance/Binance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Binance.Api
+{
+    /// <summary>
+    /// Binance API のラッパー
+    /// </summary>
+    public class Binance
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="apiKey"></param>
+        /// <param name="secretKey"></param>
+        public Binance(string apiKey, string secretKey)
+        {
+            ApiKey = apiKey;
+            SecretKey = secretKey;
+        }
+
+        /// <summary>
+        /// Binance API のキー
+        /// </summary>
+        private string ApiKey { get; set; }
+
+        /// <summary>
+        /// Binance API のシークレットキー
+        /// </summary>
+        private string SecretKey { get; set; }
+    }
+}
