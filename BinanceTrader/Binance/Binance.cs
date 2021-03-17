dac1b1981b66cd36563a80c080bed4c5efddcb6f
wertrain <wertrain@gmail.com>
2021-03-17 23:55:09 +0900

    API を更新

--- BinanceTrader/Binance/Binance.cs
+++ BinanceTrader/Binance/Binance.cs
@@ -1,18 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
 
 namespace Binance.Api
 {
     /// <summary>
+    /// 
+    /// </summary>
+    public class BinanceApiException : Exception
+    {
+        /// <summary>
+        /// ステータスコード
+        /// </summary>
+        public HttpStatusCode StatusCode { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="statusCode"></param>
+        public BinanceApiException(HttpStatusCode statusCode) => StatusCode = statusCode;
+    }
+
+    /// <summary>
     /// Binance API のラッパー
     /// </summary>
-    public class Binance
+    public class Client
     {
+        /// <summary>
+        /// エンドポイント
+        /// </summary>
+        private readonly List<string> EndPoints = new List<string>()
+        {
+            "https://api.binance.com",
+            "https://api1.binance.com",
+            "https://api2.binance.com",
+            "https://api3.binance.com",
+        };
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Client()
+        {
+
+        }
+
         /// <summary>
         /// コンストラクタ
         /// </summary>
         /// <param name="apiKey"></param>
         /// <param name="secretKey"></param>
-        public Binance(string apiKey, string secretKey)
+        public Client(string apiKey, string secretKey)
         {
             ApiKey = apiKey;
             SecretKey = secretKey;
@@ -27,5 +66,41 @@ namespace Binance.Api
         /// Binance API のシークレットキー
         /// </summary>
         private string SecretKey { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string GetEntPoint()
+        {
+            return EndPoints[0];
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string GetAllTickers()
+        {
+            var api = "/api/v3/ticker/price";
+
+            var request = WebRequest.Create(GetEntPoint() + api) as HttpWebRequest;
+            request.Method = "GET";
+            request.ContentType = "application/json;";
+
+            var httpResponse = request.GetResponse() as HttpWebResponse;
+
+            if (httpResponse.StatusCode == HttpStatusCode.OK)
+            {
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+            else
+            {
+                throw new BinanceApiException(httpResponse.StatusCode);
+            }
+        }
     }
 }
