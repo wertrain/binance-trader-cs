11e4aed365cc7860464c9f4ba933b5be997768ac
wertrain <wertrain@gmail.com>
2021-03-18 23:26:10 +0900

    WIP

---
+++ BinanceTrader/BinanceTrader/BinanceApiManager.cs
@@ -0,0 +1,26 @@
+﻿namespace BinanceTrader
+{
+    /// <summary>
+    /// Binance API 管理
+    /// </summary>
+    class BinanceApiManager
+    {
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        public static BinanceApiManager Instance { get; } = new BinanceApiManager();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Binance.Api.Client Client { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        private BinanceApiManager()
+        {
+            Client = new Binance.Api.Client();
+        }
+    }
+}
