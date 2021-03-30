103a8ee3cb0523d3f10168ed6d167e3875b111ad
wertrain <wertrain@gmail.com>
2021-03-31 01:06:18 +0900

    ダミーデータの読み込みに対応

--- BinanceTrader/Binance/Binance.cs
+++ BinanceTrader/Binance/Binance.cs
@@ -58,6 +58,11 @@ namespace Binance.Api
         }
 
         /// <summary>
+        /// ダミーデータを使用するかどうか
+        /// </summary>
+        public bool IsUseDummyData { get; set; }
+
+        /// <summary>
         /// Binance API のキー
         /// </summary>
         private string ApiKey { get; set; }
@@ -77,11 +82,16 @@ namespace Binance.Api
         }
 
         /// <summary>
-        /// 
+        /// 全てのコインの価格を取得
         /// </summary>
         /// <returns></returns>
         public string GetAllTickers()
         {
+            if (IsUseDummyData)
+            {
+                return readDummyPrice();
+            }
+
             var api = "/api/v3/ticker/price";
 
             var request = WebRequest.Create(GetEntPoint() + api) as HttpWebRequest;
@@ -103,8 +113,17 @@ namespace Binance.Api
             }
         }
 
+        /// <summary>
+        /// 通貨情報を取得
+        /// </summary>
+        /// <returns></returns>
         public string GetExchangeInfo()
         {
+            if (IsUseDummyData)
+            {
+                return readDummyExchangeInfo();
+            }
+
             var api = "/api/v3/exchangeInfo";
 
             var request = WebRequest.Create(GetEntPoint() + api) as HttpWebRequest;
@@ -123,6 +142,32 @@ namespace Binance.Api
             else
             {
                 throw new BinanceApiException(httpResponse.StatusCode);
+            }
+        }
+
+        /// <summary>
+        /// 開発用ダミーデータをファイルから読み込み
+        /// </summary>
+        /// <returns></returns>
+        private string readDummyPrice()
+        {
+            var assembly = System.Reflection.Assembly.GetEntryAssembly();
+            using (StreamReader sr = new StreamReader(Path.GetDirectoryName(assembly.Location) + Path.DirectorySeparatorChar + "dummy_price.txt", System.Text.Encoding.GetEncoding("utf-8")))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// 開発用のダミーデータをファイルから読み込み
+        /// </summary>
+        /// <returns></returns>
+        private string readDummyExchangeInfo()
+        {
+            var assembly = System.Reflection.Assembly.GetEntryAssembly();
+            using (StreamReader sr = new StreamReader(Path.GetDirectoryName(assembly.Location) + Path.DirectorySeparatorChar + "dummy_exchangeinfo.txt", System.Text.Encoding.GetEncoding("utf-8")))
+            {
+                return sr.ReadToEnd();
             }
         }
     }
