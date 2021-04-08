d7701da006422858fb0d943504deb24b07a46011
wertrain <wertrain@gmail.com>
2021-04-08 22:43:26 +0900

    アラートウィンドウの修正

--- BinanceTrader/BinanceTrader/Controls/AlertList.xaml.cs
+++ BinanceTrader/BinanceTrader/Controls/AlertList.xaml.cs
@@ -1,18 +1,5 @@
 ﻿using BinanceTrader.Controls.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace BinanceTrader.Controls
 {
@@ -21,6 +8,19 @@ namespace BinanceTrader.Controls
     /// </summary>
     public partial class AlertList : ControlBase
     {
+        /// <summary>
+        /// アラート情報
+        /// </summary>
+        public class AlertInfo
+        {
+
+        }
+
+        /// <summary>
+        /// アラート情報
+        /// </summary>
+        public ObservableCollection<AlertInfo> Alerts;
+
         public AlertList()
         {
             InitializeComponent();
